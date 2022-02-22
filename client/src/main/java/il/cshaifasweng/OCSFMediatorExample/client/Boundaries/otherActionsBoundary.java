448b94300d94f8ef1f8ba8140c89f7b0c6c0f28a
sara <sarazreke@gmail.com>
2022-02-22 15:14:22 +0200

    book lob App in 8:00-10:00

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/Boundaries/otherActionsBoundary.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/Boundaries/otherActionsBoundary.java
@@ -2,6 +2,7 @@ package il.cshaifasweng.OCSFMediatorExample.client.Boundaries;
 
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalTime;
 import java.util.LinkedList;
 import java.util.ResourceBundle;
 
@@ -23,18 +24,23 @@ public class otherActionsBoundary extends Boundary {
     void LabApp(ActionEvent event) {
         LinkedList<String> choice = new LinkedList<String>();
         System.out.println("book Lab App");
-        choice.add("#LabApp");
-        Patient patient = (Patient) user_Ob.get(0);
-        String Pid = String.valueOf(patient.getId());
-        choice.add(Pid);  // adding patient Id
-        Clinic clinic = (Clinic) params.get(0);
-        String Cid = String.valueOf(clinic.getId());
-        choice.add(Cid); // adding clinic Id
-        try {
-            SimpleClient.getClient().sendToServer(choice);
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+        if (LocalTime.now().isAfter(LocalTime.of(7, 59, 59)) && LocalTime.now().isBefore(LocalTime.of(10, 0, 0)))
+        {choice.add("#LabApp");
+            Patient patient = (Patient) user_Ob.get(0);
+            String Pid = String.valueOf(patient.getId());
+            choice.add(Pid);  // adding patient Id
+            Clinic clinic = (Clinic) params.get(0);
+            String Cid = String.valueOf(clinic.getId());
+            choice.add(Cid); // adding clinic Id
+            try {
+                SimpleClient.getClient().sendToServer(choice);
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }}
+        else
+        {
+            MessageBoundary.displayError("reception time for Laboratory is from 8:00 to 10:00");
         }
 
     }
